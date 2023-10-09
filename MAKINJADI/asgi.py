fe94259f54f1dcf858abae24151a50990966f183
Leonard Yulianus <leonard.yulianus@gmail.com>
2023-10-09 21:38:04 +0700

    Start a Django project

---
+++ MAKINJADI/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for MAKINJADI project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MAKINJADI.settings')
+
+application = get_asgi_application()
