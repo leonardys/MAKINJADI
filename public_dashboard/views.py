1a2a211bbb6dfa3122be4226ad49496fab6f5634
Leonard Yulianus <leonard.yulianus@gmail.com>
2023-11-01 18:43:03 +0700

    Test showing list of expense documents

--- public_dashboard/views.py
+++ public_dashboard/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from expense_tracker.models import ExpenseDocument
 
 
 def index(request):
-    return render(request, "dashboard/index.html")
+    expense_documents = ExpenseDocument.objects.order_by("date")
+    context = {"expense_documents": expense_documents}
+    return render(request, "dashboard/index.html", context)
