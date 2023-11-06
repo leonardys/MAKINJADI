9881e6de7072d96edb1eebccb7bb5d12d1c35fc3
Leonard Yulianus <leonard.yulianus@gmail.com>
2023-11-06 19:57:01 +0700

    Create basic list view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/templates/*.{html,js}"],
+  content: ["./**/templates/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
 }
