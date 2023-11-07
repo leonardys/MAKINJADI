598758fbb86397157b98af658d32426c6de77510
Leonard Yulianus <leonard.yulianus@gmail.com>
2023-11-07 18:16:48 +0700

    Add detail page

--- public_dashboard/views.py
+++ public_dashboard/views.py
@@ -1,14 +1,19 @@
 from django.views import generic
 from expense_tracker.models import ExpenseDocument
-from django.db.models import Max
+from django.db import models
 
 
 class IndexView(generic.ListView):
-    model = ExpenseDocument
     template_name = "dashboard/index.html"
     context_object_name = "expense_documents"
 
     def get_queryset(self):
         return ExpenseDocument.objects.annotate(
-            last_status_update=Max("logs__created_at")
+            last_status_update=models.Max("logs__created_at")
         ).order_by("-last_status_update", "date", "work_order__number")
+
+
+class DetailView(generic.DetailView):
+    template_name = "dashboard/detail.html"
+    context_object_name = "expense_document"
+    queryset = ExpenseDocument.objects.all()
