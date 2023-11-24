197a4c76ec322d7a611b27760099e134665c7a73
Leonard Yulianus <leonard.yulianus@gmail.com>
2023-11-24 14:56:48 +0700

    Change status update into timeline

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,6 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "bumblebee"],
+    themes: ["bumblebee"],
   },
 }
